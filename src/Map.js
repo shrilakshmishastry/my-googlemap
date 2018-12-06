188f89fdd0098b40a042151ef7449211b915752a
shrilakshmishastry <shrilakshmishastry@gmail.com>
2018-12-06 09:26:27 +0530

    new

--- src/Map.js
+++ src/Map.js
@@ -9,79 +9,82 @@ const mapStyles = {
   }
 };
 
-export class CurrentLocation extends React.Component{
-constructor(props){
-  super(props);
-  const {lat,lng} = this.props.initialCenter;
-  this.state = {
-    currentLocation:{
-      lat:lat,
-      lng:lng
-    }
-  };
-}
-componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
-    }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
-    }
-  }
-  recenterMap() {
-      const map = this.map;
-      const current = this.state.currentLocation;
+export class CurrentLocation extends React.Component {
+  constructor(props) {
+    super(props);
 
-      const google = this.props.google;
-      const maps = google.maps;
-
-      if (map) {
-        let center = new maps.LatLng(current.lat, current.lng);
-        map.panTo(center);
+    const { lat, lng } = this.props.initialCenter;
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng
       }
-    }
-    componentDidMount() {
-        if (this.props.centerAroundCurrentLocation) {
-          if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(pos => {
-              const coords = pos.coords;
-              this.setState({
-                currentLocation: {
-                  lat: coords.latitude,
-                  lng: coords.longitude
-                }
-              });
-            });
-          }
-        }
+    };
+  }
+  componentDidUpdate(prevProps, prevState) {
+      if (prevProps.google !== this.props.google) {
         this.loadMap();
       }
-loadMap() {
-    if (this.props && this.props.google) {
-      // checks if google is available
-      const { google } = this.props;
-      const maps = google.maps;
-
-      const mapRef = this.refs.map;
-
-      // reference to the actual DOM element
-      const node = ReactDOM.findDOMNode(mapRef);
-
-      let { zoom } = this.props;
-      const { lat, lng } = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
-      const mapConfig = Object.assign(
-        {},
-        {
-          center: center,
-          zoom: zoom
-        }
-      );
-
-      // maps.Map() is constructor that instantiates the map
-      this.map = new maps.Map(node, mapConfig);
+      if (prevState.currentLocation !== this.state.currentLocation) {
+        this.recenterMap();
+      }
     }
-  }
+    recenterMap() {
+     const map = this.map;
+     const current = this.state.currentLocation;
+
+     const google = this.props.google;
+     const maps = google.maps;
+
+     if (map) {
+       let center = new maps.LatLng(current.lat, current.lng);
+       map.panTo(center);
+     }
+   }
+   componentDidMount() {
+      if (this.props.centerAroundCurrentLocation) {
+        if (navigator && navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(pos => {
+            const coords = pos.coords;
+            this.setState({
+              currentLocation: {
+                lat: coords.latitude,
+                lng: coords.longitude
+              }
+            });
+          });
+        }
+      }
+      this.loadMap();
+    }
+
+
+    loadMap() {
+        if (this.props && this.props.google) {
+          // checks if google is available
+          const { google } = this.props;
+          const maps = google.maps;
+
+          const mapRef = this.refs.map;
+
+          // reference to the actual DOM element
+          const node = ReactDOM.findDOMNode(mapRef);
+
+          let { zoom } = this.props;
+          const { lat, lng } = this.state.currentLocation;
+          const center = new maps.LatLng(lat, lng);
+          const mapConfig = Object.assign(
+            {},
+            {
+              center: center,
+              zoom: zoom
+            }
+          );
+
+          // maps.Map() is constructor that instantiates the map
+          this.map = new maps.Map(node, mapConfig);
+        }
+      }
   renderChildren() {
     const { children } = this.props;
 
@@ -115,12 +118,11 @@ render(){
 export default CurrentLocation;
 
 CurrentLocation.defaultProps = {
-    zoom:14,
-    initialCenter:{
-      lat:-1.2884,
-      lng:36.888
-    },
-    centerAroundCurrentLocation:false,
-    visible:true
-
+  zoom: 14,
+  initialCenter: {
+    lat: -1.2884,
+    lng: 36.8233
+  },
+  centerAroundCurrentLocation: false,
+  visible: true
 };
